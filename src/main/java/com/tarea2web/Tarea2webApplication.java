31ecc8100e11a75b608c7ab6b6961f30587e50bd
edgar <edgar@DESKTOP-CKEBD8Q>
2023-06-08 23:21:59 -0600

    Tarea 2 Desarrollo web

---
+++ src/main/java/com/tarea2web/Tarea2webApplication.java
@@ -0,0 +1,13 @@
+package com.tarea2web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tarea2webApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tarea2webApplication.class, args);
+	}
+
+}
